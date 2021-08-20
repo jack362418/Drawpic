de5c995b1fb8cfcac05c9b5ea8c9d5c6ff9140e6
Jack <O-JIANGHUIYANG>
2021-08-20 09:51:41 +0800

    修改方向

--- src/views/middleCanvas/createElementWraper.vue
+++ src/views/middleCanvas/createElementWraper.vue
@@ -3,8 +3,7 @@
     ref="creatElementRef"
     @mousedown.stop="$event => createElement($event)"
   >
-
-    <div class="createBox" :style="{
+    <div class="createBox" v-if="isStartRef" :style="{
         width: elementPositionRef.width + 'px',
         height: elementPositionRef.height + 'px',
         top: elementPositionRef.y + 'px',
@@ -20,48 +19,72 @@ import { useStore } from '@/store'
 export default defineComponent({
   name: 'creatElement',
   setup() {
-      const store = useStore()
-      const elementPositionRef = ref({
-          width:0,
-          height:0,
-          x:0,
-          y:0
-      })
-
-      const el = reactive({
-          x:0,
-          y:0
-      })
-      
-      const creatElementRef = ref<HTMLElement>()
-      onMounted(() => {
-         if (!creatElementRef.value) return
-         let {x,y} = creatElementRef.value.getBoundingClientRect()
-         el.x = x
-         el.y = y
-      })  
-
-      const createElement = (e:MouseEvent) => {
-        let clientX = e.clientX
-        let clientY = e.clientY
-        elementPositionRef.value.x = clientX - el.x
-        elementPositionRef.value.y = clientY - el.y
-        document.onmousemove = (e:MouseEvent) => {
-             elementPositionRef.value.width =  e.clientX - clientX
-             elementPositionRef.value.height = e.clientY - clientY
+        const store = useStore()
+        const elementPositionRef = ref({
+            width:0,
+            height:0,
+            x:0,
+            y:0
+        })
+        const isStartRef = ref<boolean>(false)
+        const el = reactive({
+            x:0,
+            y:0
+        })
+        const creatElementRef = ref<HTMLElement>()
+        onMounted(() => {
+            if (!creatElementRef.value) return
+            let {x,y} = creatElementRef.value.getBoundingClientRect()
+            el.x = Math.floor(x)
+            el.y = Math.floor(y) 
+        })  
+        
+        const createElement = (e:MouseEvent) => {
+            isStartRef.value = true
+            let clientX = e.clientX
+            let clientY = e.clientY
+            elementPositionRef.value.x = clientX - el.x
+            elementPositionRef.value.y = clientY - el.y
+            /**
+             * 上下左右均可移动
+             */
+            document.onmousemove = (e:MouseEvent) => {
+                elementPositionRef.value.width = Math.abs(e.clientX - clientX) 
+                elementPositionRef.value.height = Math.abs(e.clientY - clientY) 
+                if(e.clientX > clientX && e.clientY < clientY) {
+                  elementPositionRef.value.y = e.clientY - el.y
+                }
+                if(e.clientX < clientX && e.clientY > clientY) {
+                  elementPositionRef.value.x = e.clientX - el.x
+                }
+                if(e.clientX < clientX && e.clientY < clientY) {
+                  elementPositionRef.value.x = e.clientX - el.x
+                  elementPositionRef.value.y = e.clientY - el.y
+                }
+                if(e.clientX > clientX && e.clientY > clientY) {
+                  elementPositionRef.value.x = clientX - el.x
+                  elementPositionRef.value.y = clientY - el.y
+                }
+                if(e.clientX == clientX) {
+                   elementPositionRef.value.x = clientX - el.x
+                }
+                if(e.clientY == clientY) {
+                  elementPositionRef.value.y = clientY - el.y
+                }
+            }
+            document.onmouseup = (e:MouseEvent) => {
+                document.onmousemove = null
+                document.onmouseup = null
+                store.commit("CANCEL_CREATE_EL",false)
+            }
         }
-        document.onmouseup = (e:MouseEvent) => {
-            document.onmousemove = null
-            document.onmouseup = null
-            store.commit("CANCEL_CREATE_EL",false)
-        }
-      }
 
-      return {
-          createElement,
-          creatElementRef,
-          elementPositionRef
-      }
+        return {
+            createElement,
+            creatElementRef,
+            elementPositionRef,
+            isStartRef
+        }
   }
 })
 </script>
